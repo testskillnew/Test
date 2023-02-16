98c8ddff63077e56da67d7fee360f604aad1a1aa
TorusTredent <torus.tredent@gmail.com>
2023-02-16 19:42:22 +0300

    text

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
